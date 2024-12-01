36ea984749b2363efc8ef1a98e36649186df6990
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-12-01 03:36:40 -0500

    completed UI Setup

--- pomodoro.py
+++ pomodoro.py
@@ -10,8 +10,12 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset():
+    pass
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start():
+    pass
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -19,13 +23,31 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer")
+timer_label.config(bg=YELLOW, fg= GREEN,font=(FONT_NAME, 40))
+timer_label.grid(column=1, row=0)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 bg_image = PhotoImage(file="tomato.png")  # convert image to canvas image format
-canvas.create_image(103, 112, image=bg_image) 
-canvas.pack()
+canvas.create_image(100, 112, image=bg_image) 
+canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 40, "bold"))
+canvas.grid(column=1, row=1)
+
+
+start_button = Button(text="Start", highlightbackground=YELLOW,command = start)
+start_button.grid(column=0, row=2)
+
+
+reset_button = Button(text="Reset", highlightbackground=YELLOW,command = reset)
+reset_button.grid(column=2, row=2)
+
+check_box_label = Label(text="✅")
+check_box_label.config(bg=YELLOW, fg= GREEN,font=(FONT_NAME, 15))
+check_box_label.grid(column=1, row=3)
+
+
 
 
 
